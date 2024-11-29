f84106f759b50141daa2a2ff39ebfc3edfb0550f
huangliangbo <864181495@qq.com>
2024-11-29 10:59:38 +0800

    first commit

---
+++ apps/web/src/main.ts
@@ -0,0 +1,24 @@
+import { Header } from "@repo/ui/header";
+import "./style.css";
+import typescriptLogo from "/typescript.svg";
+import { Counter } from "@repo/ui/counter";
+import { setupCounter } from "@repo/ui/setup-counter";
+import { add } from "@repo/math/add";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    ${Header({ title: "Web" })}
+    <div class="card">
+      ${Counter()}
+      ${add(5,10)}
+    </div>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
